578b9665dfabbb55b484bfbb2b85d9b08ee9c809
praizerskater <tomruffcodes@gmail.com>
2019-01-20 17:24:13 -0700

    stuck, going to work on basics

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// need to make a event listener to know when someone clicks the button
+var click = document.addEventListener('click', (e) => console.log(e));
+
+// need to make a function that when the button does get clicked it sends an output to the user
+
+let output = []; 
+
+var yourNumber = () => {
+    for (i = 0, i <= 10, i++) {
+        output =[i];
+    }
+}
+console.log(yourNumber);
